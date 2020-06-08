51509035ce918db2d2e43c935ef6f1dd3f77c9d0
GreenhillTeacher <suarezm@greenhill.org>
2020-06-08 15:36:41 -0500

    This  is addition of main function

---
+++ src/com/suarez/Calculation.java
@@ -0,0 +1,7 @@
+package com.suarez;
+
+public class Calculation {
+    public static void main(String [] args){
+        
+    }
+}
--- src/com/suarez/Expenses.java
+++ src/com/suarez/Expenses.java
@@ -3,6 +3,9 @@ package com.suarez;
 public class Expenses {
     //ASasAS
     public static void main(String [] args){
-        
+        int bill1=30;
+        int subtotal= bill1+48+32;
+        double taxes =subtotal*0.08;
+
     }
 }
